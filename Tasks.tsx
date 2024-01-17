9150b4d4acd5df9ec610d73e637b53f48184351a
Justapickel <143851370+Justapickel@users.noreply.github.com>
2024-01-17 12:26:16 -1000

    Update Tasks.tsx

--- Tasks.tsx
+++ Tasks.tsx
@@ -4,27 +4,21 @@ export default function FirstPost() {
   return (
     <>
       <h1>Your Tasks, Should You Choose To Accept Them:</h1>
+      <br></br>
       <h3>
       Say hello when you arrive: 10 points
       </h3>
-      <h3>
-Be Respectful to Another person: 15 points
-      </h3>
-      <h3>
-      Clean up behind yourself: 40 points
-      </h3>
-      <h3>
-      Keep fires small: 10 points
-      </h3>
-      <h3>
-      Respect people’s private property: 10 points
-      </h3>
-      <h3>
-      Help to protect endangered species: 100 points
-      </h3>
-      <h2>
-        <Link href="/Lobby">Back to Lobby</Link>
-      </h2>
+      <br></br>
+      <h3>Be Respectful to Another person: 15 points</h3>
+      <br></br>
+      <h3>Clean up behind yourself: 40 points</h3>
+      <br></br>
+      <h3>Keep fires small: 10 points</h3>
+      <br></br>
+      <h3>Respect people’s private property: 10 points</h3>
+      <br></br>
+      <h3>Help to protect endangered species: 100 points</h3>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      <h2><Link href="/Lobby">Back to Lobby</Link></h2>
     </>
   );
-}
