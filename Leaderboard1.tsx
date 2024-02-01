08f69a22590f6146dc984914dbac6e3a810e4e50
Justapickel <143851370+Justapickel@users.noreply.github.com>
2024-02-01 13:08:49 -1000

    Update Leaderboard1.tsx
    I made it good

--- Leaderboard1.tsx
+++ Leaderboard1.tsx
@@ -1,16 +1,22 @@
 import Link from 'next/link';
-
+import React from "react";
 export default function FirstPost() {
   return (
     <>
-      <h1>Leaderboard</h1>
-      <h2>1st: Urdad123: 1613614767426741617 points</h2>
-      <h2>2nd:SussiestOfTheBakas: 4 points</h2>
-      <h2>3rd: mmmmm: 3 points</h2>
+    <div style={{ fontSize: 15 }}> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Your Current Points: 0</div>
+      <h1 className="date" style={{ fontSize: 50}}>Leaderboard</h1>
+      <br></br>
+      <h2 className="first" style={{ fontSize: 20}}>1st: Urdad123: 1613614767426741617 points</h2>
+      <br></br>
+      <h2 className="second" style={{ fontSize: 20}}>2nd:SussiestOfTheBakas: 4 points</h2>
+      <br></br>
+      <h2 className="third" style={{ fontSize: 20}}>3rd: mmmmm: 3 points</h2>
+      <br></br>
+      <br></br>
+      <br></br>
       <h2>888th: Chris P. Bacon (you): 0 points</h2>
-      <h2>
-        <Link href="/Lobby">Back to home</Link>
-      </h2>
+      <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+      <h2><Link href="/Lobby">Back to Lobby</Link></h2>
     </>
   );
 }
