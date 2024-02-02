a009bb02ca5e091db7721bebdbf191dce02245bd
NeedToGitALife <150964788+NeedToGitALife@users.noreply.github.com>
2024-02-02 13:38:09 -1000

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,14 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      Image: {
+      'sussy-lol': "url(https://geology.com/state-map/maps/hawaii-county-map.gif)",
+      },
+
+      backgroundImage: {
+      'sussy-forest': "url('https://cdn.britannica.com/99/94299-050-4FD6184B/Hawaii-cliffs-state-waters-Tourists-Pacific-Ocean.jpg')",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 } satisfies Config;
